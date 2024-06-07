7818e9bacb2d0d22f86fad0649ad64590c31297b
MiguelR222 <miguel.ruiz0652@gmail.com>
2024-06-06 20:28:22 -0600

    alert when playlist added

--- components/toggleBox.js
+++ components/toggleBox.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect} from 'react';
 import usePostPlaylist from "@/hooks/usePostPlaylist"
 import { useSession } from "next-auth/react";
 import usePostTracks from '@/hooks/usePostTracks';
@@ -14,6 +14,13 @@ export default function ToggleBox({ recommendations }) {
   const toggleVisibility = () => {
     setIsVisible(!isVisible);
   };
+
+  useEffect(() => {
+    if (playlist && postTracks) {
+      alert('Playlist added to your library');
+    }
+  }, [playlist, postTracks]);
+
 
 return (
   <div className="mt-3 relative">
@@ -42,7 +49,7 @@ return (
             />
             <button
               className="px-12 py-3 bg-[#014017] text-[#F2F2F2] rounded shadow hover:bg-[#41BFB3] text-sm font-medium focus:outline-none focus:ring active:bg-[#41BFB3] sm:w-auto transition transform hover:scale-105 hover:rotate-3"
-            >
+              onClick={() => { setPlaylistName((playlistInput)) }}>
               Add
             </button>
           </div>
